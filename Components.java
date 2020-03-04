ea36a172eef106480a1c2347cc79179933c2cdef
Alvyn Berg <alvynb@pm.me>
2020-03-04 10:28:40 -0500

    Initial commit

---
+++ Components.java
@@ -0,0 +1,27 @@
+/* Circuit Simulator --- Components.java
+ *
+ * @author Alvyn Berg
+ *
+ * Comopnents class, containing definitions for all objects used in basic 
+ * circuits: breadboard, voltage source, ground, conductive wires, resistors,
+ * capacitors, LEDs, potentiometers, and buttons. */
+
+// learn to work with packages.
+
+/* Breadboard class. 
+ * This first iteration will use an adjacency matrix (2D array)
+ * */
+public class Socket {
+	// create generic types. 
+	// We will connect anything to the sockets. 
+}
+
+public class Breadboard {
+	 private char[][] board;
+
+	 public Breadboard(int rows, int columns) {
+		 this.board = new char[rows][columns];
+	 }
+
+}
+
